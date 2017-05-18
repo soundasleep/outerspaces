ccf38bcdbe58a9e88f04691ea224043fecad06fd
Jevon Wright <jevon@powershop.co.nz>
2017-05-19 11:41:04 +1200

    Use mysql2 instead of sqlite3

--- db/schema.rb
+++ db/schema.rb
@@ -10,70 +10,68 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170517234826) do
+ActiveRecord::Schema.define(version: 20170518233713) do
 
-  create_table "railswiki_histories", force: :cascade do |t|
-    t.integer  "page_id"
-    t.text     "body"
-    t.integer  "author_id"
+  create_table "railswiki_histories", id: :integer, force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.integer "page_id", null: false
+    t.text "body"
+    t.integer "author_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["author_id"], name: "index_railswiki_histories_on_author_id"
     t.index ["page_id"], name: "index_railswiki_histories_on_page_id"
   end
 
-  create_table "railswiki_invites", force: :cascade do |t|
-    t.string   "email",            null: false
+  create_table "railswiki_invites", id: :integer, force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.string "email", null: false
     t.datetime "accepted_at"
-    t.integer  "inviting_user_id", null: false
-    t.integer  "invited_user_id"
-    t.datetime "created_at",       null: false
-    t.datetime "updated_at",       null: false
-    t.string   "role"
+    t.integer "inviting_user_id", null: false
+    t.integer "invited_user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "role", default: "user", null: false
     t.index ["invited_user_id"], name: "index_railswiki_invites_on_invited_user_id"
     t.index ["inviting_user_id"], name: "index_railswiki_invites_on_inviting_user_id"
   end
 
-  create_table "railswiki_pages", force: :cascade do |t|
-    t.string   "title"
-    t.integer  "latest_version_id"
-    t.datetime "created_at",        null: false
-    t.datetime "updated_at",        null: false
-    t.string   "lowercase_title"
-    t.index ["latest_version_id"], name: "index_railswiki_pages_on_latest_version_id"
-    t.index ["lowercase_title"], name: "index_railswiki_pages_on_lowercase_title", unique: true
-    t.index ["title"], name: "index_railswiki_pages_on_title", unique: true
-  end
-
-  create_table "railswiki_uploaded_files", force: :cascade do |t|
-    t.string   "file"
-    t.integer  "user_id"
+  create_table "railswiki_pages", id: :integer, force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.string "title", null: false
+    t.integer "latest_version_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string   "title"
+    t.string "lowercase_title", null: false
+    t.index ["latest_version_id"], name: "index_railswiki_pages_on_latest_version_id"
+  end
+
+  create_table "railswiki_uploaded_files", id: :integer, force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.string "file"
+    t.integer "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "title"
     t.index ["user_id"], name: "index_railswiki_uploaded_files_on_user_id"
   end
 
-  create_table "railswiki_users", force: :cascade do |t|
-    t.string   "provider"
-    t.string   "uid"
-    t.string   "name"
-    t.string   "refresh_token"
-    t.string   "access_token"
-    t.datetime "expires"
-    t.datetime "created_at",    null: false
-    t.datetime "updated_at",    null: false
-    t.string   "email"
-    t.string   "image_url"
-    t.datetime "last_login"
-    t.string   "role"
+  create_table "railswiki_users", id: :integer, force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.string "provider"
+    t.string "uid"
+    t.string "name"
+    t.string "refresh_token"
+    t.string "access_token"
+    t.timestamp "expires"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "email"
+    t.string "image_url"
+    t.timestamp "last_login"
+    t.string "role"
     t.index ["email"], name: "index_railswiki_users_on_email", unique: true
     t.index ["role"], name: "index_railswiki_users_on_role"
   end
 
-  create_table "sessions", force: :cascade do |t|
-    t.string   "session_id", null: false
-    t.text     "data"
+  create_table "sessions", id: :integer, force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.string "session_id", null: false
+    t.text "data"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["session_id"], name: "index_sessions_on_session_id", unique: true
